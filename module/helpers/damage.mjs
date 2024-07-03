6e51078f2cc8f0dfaa0cb6d5d92a61b3f4b116f5
Niklas Schönberg <niklas@uplink.fi>
2024-07-04 02:25:55 +0300

    fix: hopefully got all the custom rolls.

--- module/helpers/damage.mjs
+++ module/helpers/damage.mjs
@@ -1,145 +1,151 @@
-import {usr} from "./config.mjs";
+import { usr } from "./config.mjs";
 
-export function removeStun (actor) {
-    const health = actor.system.health;
-    if (health.x > 0) {
-        health.x--;
-        actor.update({"system.health": health});
-    }
+export function removeStun(actor) {
+  const health = actor.system.health;
+  if (health.x > 0) {
+    health.x--;
+    actor.update({ "system.health": health });
+  }
 }
 
 export function addHealingPoints(actor) {
-    const data = {};
-    renderTemplate('systems/usr/templates/helpers/heal-dialog.hbs', data).then(content => {
-        let d = new Dialog({
-            title: "Add Healing Points",
-            content,
-            buttons: {
-                heal: {
-                    icon: '<i class="fas fa-mortar-pestle"></i>',
-                    label: "Heal",
-                    callback: (html) => {
-                        let total = 0;
-                        const health = actor.system.health;
-                        health.hp = (health.hp ?? 0) + parseInt(html.find("#add-hp")[0].value ?? '0');
-                        if (health.hp > 0) {
-                            Object.keys(usr.wounds).forEach(key => {
-                                const wound = usr.wounds[key];
-                                if (wound.hp > 0) {
-                                    let nr = health[key];
-                                    if (nr > 0) {
-                                        let healNr = Math.floor(health.hp / wound.hp);
-                                        if (healNr > nr) healNr = nr;
-                                        health[key] -= healNr;
-                                        health.hp -= healNr * wound.hp;
-                                    }
-                                } else {
-                                    health[key] = 0;
-                                }
-                                total += health[key];
-                            });
-                        }
-                        if (total < 1) {
-                            // Zero HP if no more damage.
-                            health.hp = 0;
-                        }
-
-                        actor.update({"system.health": health});
+  const data = {};
+  renderTemplate("systems/usr/templates/helpers/heal-dialog.hbs", data).then(
+    (content) => {
+      let d = new Dialog({
+        title: "Add Healing Points",
+        content,
+        buttons: {
+          heal: {
+            icon: '<i class="fas fa-mortar-pestle"></i>',
+            label: "Heal",
+            callback: (html) => {
+              let total = 0;
+              const health = actor.system.health;
+              health.hp =
+                (health.hp ?? 0) +
+                parseInt(html.find("#add-hp")[0].value ?? "0");
+              if (health.hp > 0) {
+                Object.keys(usr.wounds).forEach((key) => {
+                  const wound = usr.wounds[key];
+                  if (wound.hp > 0) {
+                    let nr = health[key];
+                    if (nr > 0) {
+                      let healNr = Math.floor(health.hp / wound.hp);
+                      if (healNr > nr) healNr = nr;
+                      health[key] -= healNr;
+                      health.hp -= healNr * wound.hp;
                     }
-                }
+                  } else {
+                    health[key] = 0;
+                  }
+                  total += health[key];
+                });
+              }
+              if (total < 1) {
+                // Zero HP if no more damage.
+                health.hp = 0;
+              }
+
+              actor.update({ "system.health": health });
             },
-            default: "heal",
-        });
-        d.options.classes = ["usr", "dialog", "heal"];
-        d.render(true);
-    });
+          },
+        },
+        default: "heal",
+      });
+      d.options.classes = ["usr", "dialog", "heal"];
+      d.render(true);
+    }
+  );
 }
 
 export function addDamage(actor) {
-
-    const data = {
-        wounds: usr.wounds
-    };
-    renderTemplate('systems/usr/templates/helpers/damage-dialog.hbs', data).then(content => {
-        let d = new Dialog({
-            classes: ["usr", "dialog", "damage"],
-            title: "Take Damage",
-            content,
-            buttons: {
-                damage: {
-                    icon: '<i class="fas fa-burst"></i>',
-                    label: "Damage",
-                    callback: (html) => {
-                        const args = getArgs(html);
-                        setDamage(args.amount,args.type,actor);
-                    }
-                },
-                resist: {
-                    icon: '<i class="fas fa-person-burst"></i>',
-                    label: "Resist",
-                    callback: (html) => {
-                        const args = getArgs(html);
-                        resistDamage(args.amount,args.type,actor);
-                    }
-                }
+  const data = {
+    wounds: usr.wounds,
+  };
+  renderTemplate("systems/usr/templates/helpers/damage-dialog.hbs", data).then(
+    (content) => {
+      let d = new Dialog({
+        classes: ["usr", "dialog", "damage"],
+        title: "Take Damage",
+        content,
+        buttons: {
+          damage: {
+            icon: '<i class="fas fa-burst"></i>',
+            label: "Damage",
+            callback: (html) => {
+              const args = getArgs(html);
+              setDamage(args.amount, args.type, actor);
             },
-            default: "resist",
-        });
-        d.options.classes = ["usr", "dialog", "damage"];
-        d.render(true);
-    });
+          },
+          resist: {
+            icon: '<i class="fas fa-person-burst"></i>',
+            label: "Resist",
+            callback: (html) => {
+              const args = getArgs(html);
+              resistDamage(args.amount, args.type, actor);
+            },
+          },
+        },
+        default: "resist",
+      });
+      d.options.classes = ["usr", "dialog", "damage"];
+      d.render(true);
+    }
+  );
 }
 
 function getArgs(html) {
-    const type = html.find("#wound")[0].value ?? 'x'
-    const amount = parseInt(html.find("#add-damage")[0].value ?? '0')
+  const type = html.find("#wound")[0].value ?? "x";
+  const amount = parseInt(html.find("#add-damage")[0].value ?? "0");
 
-    return {type, amount};
+  return { type, amount };
 }
 
-function resistDamage(amount,type,actor) {
-    const resist = actor.system.damage.resistance[type];
-    const wound = usr.wounds[type];
+function resistDamage(amount, type, actor) {
+  const resist = actor.system.damage.resistance[type];
+  const wound = usr.wounds[type];
 
-    const roll = new Roll(`${amount}d10cs<=${resist}`);
-    roll.evaluate({async: false});
-
+  new Roll(`${amount}d10cs<=${resist}`).evaluate().then((roll) => {
+    console.log(roll);
     const label = `Resisting ${amount} ${wound.label} damage.`;
 
     roll.toMessage({
-        speaker: ChatMessage.getSpeaker({actor}),
-        flavor: label,
-        rollMode: game.settings.get("core", "rollMode"),
+      speaker: ChatMessage.getSpeaker({ actor }),
+      flavor: label,
+      rollMode: game.settings.get("core", "rollMode"),
     });
 
     let remaining = amount - roll.total;
 
     if (remaining > 0) {
-        setDamage(remaining,type,actor);
+      setDamage(remaining, type, actor);
     }
+  });
 }
 
-function setDamage(amount,type,actor) {
-    const health = actor.system.health;
-    const wound = usr.wounds[type];
-    health[type] += amount;
+function setDamage(amount, type, actor) {
+  const health = actor.system.health;
+  const wound = usr.wounds[type];
+  health[type] += amount;
 
-    actor.update({"system.health": health});
+  actor.update({ "system.health": health });
 
-    const speaker = ChatMessage.getSpeaker({actor});
+  const speaker = ChatMessage.getSpeaker({ actor });
 
-    const content = `${amount} boxes of ${wound.label} damage.`;
+  const content = `${amount} boxes of ${wound.label} damage.`;
 
-    // Prepare chat data
-    const messageData = {
-        user: game.user.id,
-        content,
-        speaker,
-        flavor: "Received Damage",
-    };
+  // Prepare chat data
+  const messageData = {
+    user: game.user.id,
+    content,
+    speaker,
+    flavor: "Received Damage",
+  };
 
-    const msg = new ChatMessage(messageData);
+  const msg = new ChatMessage(messageData);
 
-    ChatMessage.create(msg.toObject(), {rollMode: game.settings.get("core", "rollMode")});
+  ChatMessage.create(msg.toObject(), {
+    rollMode: game.settings.get("core", "rollMode"),
+  });
 }
-
--- module/helpers/roll.mjs
+++ module/helpers/roll.mjs
@@ -24,112 +24,111 @@ export function usrRoll(data) {
   }
   const nr = Math.abs(data.difficulty);
 
-  const roll = new Roll(`${nr}d10`);
-  roll.evaluate({ async: false });
+  new Roll(`${nr}d10`).evaluate().then((roll) => {
+    const result = {
+      difficulty: data.difficulty,
+      skill: data.skill,
+      specialization: data.specialization,
+      type: "d10",
+      dice: [],
+      successes: 0,
+      critical: false,
+      formula: "",
+      total: "",
+    };
 
-  const result = {
-    difficulty: data.difficulty,
-    skill: data.skill,
-    specialization: data.specialization,
-    type: "d10",
-    dice: [],
-    successes: 0,
-    critical: false,
-    formula: "",
-    total: "",
-  };
+    let ones = -1;
+    let tens = -1;
+    let failed = false;
 
-  let ones = -1;
-  let tens = -1;
-  let failed = false;
-
-  for (const die of roll.dice[0].results) {
-    result.dice.push({
-      value: die.result,
-      success: die.result <= result.skill,
-    });
-    if (result.difficulty < 0) {
-      if (die.result > result.skill) {
-        failed = true;
+    console.log("go", roll.dice);
+    for (const die of roll.dice[0].results) {
+      console.log(die);
+      result.dice.push({
+        value: die.result,
+        success: die.result <= result.skill,
+      });
+      if (result.difficulty < 0) {
+        if (die.result > result.skill) {
+          failed = true;
+        }
+      } else {
+        if (die.result <= result.skill) {
+          result.successes++;
+        }
       }
-    } else {
-      if (die.result <= result.skill) {
+      if (die.result <= result.specialization) {
+        result.successes++;
+      }
+      if (die.result === 1) {
+        ones++;
+      }
+      if (die.result === 10) {
+        tens++;
+      }
+    }
+    if (result.difficulty < 0) {
+      if (failed) {
+        result.successes = 0;
+      } else {
         result.successes++;
       }
     }
-    if (die.result <= result.specialization) {
-      result.successes++;
-    }
-    if (die.result === 1) {
-      ones++;
-    }
-    if (die.result === 10) {
-      tens++;
-    }
-  }
-  if (result.difficulty < 0) {
-    if (failed) {
-      result.successes = 0;
-    } else {
-      result.successes++;
-    }
-  }
 
-  if (ones > 0) {
-    result.successes += ones;
-    result.critical = true;
-  }
-
-  if (tens > 0) {
-    result.successes -= tens;
-    if (result.successes < 1) {
-      result.successes = 0;
+    if (ones > 0) {
+      result.successes += ones;
       result.critical = true;
     }
-  }
 
-  result.formula = `Difficulty: ${result.difficulty} / Skill: ${result.skill}`;
-  if (result.specialization > 0) {
-    result.formula += ` (${result.specialization})`;
-  }
-  result.total =
-    (result.critical ? "Critical " : "") +
-    (result.successes ? result.successes + " Successes" : "Fail");
-
-  const speaker = ChatMessage.getSpeaker({ actor: data.actor });
-
-  showRoll(result, speaker, data.flavor);
-
-  if (!result.critical && data.trait && data.actor) {
-    let awarded = false;
-    if (data.trait) {
-      const trait = traits[data.trait];
-      if (data.spec) {
-        trait.spec.forEach((spec) => {
-          if (data.spec === spec.title) {
-            if (
-              spec.value < 3 &&
-              (spec.roll < 1 || (spec.roll < 2 && data.difficulty < 4))
-            ) {
-              awarded = true;
-              spec.roll++;
-            }
-          }
-        });
-      }
-      if (!awarded) {
-        if (
-          trait.value < 7 &&
-          (trait.roll < 1 || (trait.roll < 2 && data.difficulty < 4))
-        ) {
-          trait.roll++;
-        }
+    if (tens > 0) {
+      result.successes -= tens;
+      if (result.successes < 1) {
+        result.successes = 0;
+        result.critical = true;
       }
     }
-    data.actor.update({ "system.traits": traits });
-  }
 
-  return result;
+    result.formula = `Difficulty: ${result.difficulty} / Skill: ${result.skill}`;
+    if (result.specialization > 0) {
+      result.formula += ` (${result.specialization})`;
+    }
+    result.total =
+      (result.critical ? "Critical " : "") +
+      (result.successes ? result.successes + " Successes" : "Fail");
+
+    const speaker = ChatMessage.getSpeaker({ actor: data.actor });
+
+    showRoll(result, speaker, data.flavor);
+
+    if (!result.critical && data.trait && data.actor) {
+      let awarded = false;
+      if (data.trait) {
+        const trait = traits[data.trait];
+        if (data.spec) {
+          trait.spec.forEach((spec) => {
+            if (data.spec === spec.title) {
+              if (
+                spec.value < 3 &&
+                (spec.roll < 1 || (spec.roll < 2 && data.difficulty < 4))
+              ) {
+                awarded = true;
+                spec.roll++;
+              }
+            }
+          });
+        }
+        if (!awarded) {
+          if (
+            trait.value < 7 &&
+            (trait.roll < 1 || (trait.roll < 2 && data.difficulty < 4))
+          ) {
+            trait.roll++;
+          }
+        }
+      }
+      data.actor.update({ "system.traits": traits });
+    }
+  });
 }
 
 function showRoll(result, speaker, flavor = "") {
@@ -236,24 +235,24 @@ export function rollXp(data) {
           paid = true;
         }
         if (paid) {
-          const target = spec.value * 3 + 11;
-          const roll = new Roll("2d10");
-          roll.evaluate({ async: false });
-          if (roll.total > target) {
-            spec.xp++;
-            if (spec.xp > 2) {
-              spec.value++;
-              spec.xp -= 3;
+          const target = spec.value * 3 + 10;
+          new Roll("2d10").evaluate().then((roll) => {
+            if (roll.total > target) {
+              spec.xp++;
+              if (spec.xp > 2) {
+                spec.value++;
+                spec.xp -= 3;
+              }
             }
-          }
-          const label = `Roll for XP on ${spec.title} with value of ${spec.value}. Needs a result over ${target}.`;
-          roll.toMessage({
-            speaker: ChatMessage.getSpeaker({ actor: data.actor }),
-            flavor: label,
-            rollMode: game.settings.get("core", "rollMode"),
+            const label = `Roll for XP on ${spec.title} with value of ${spec.value}. Needs a result over ${target}.`;
+            roll.toMessage({
+              speaker: ChatMessage.getSpeaker({ actor: data.actor }),
+              flavor: label,
+              rollMode: game.settings.get("core", "rollMode"),
+            });
+            data.actor.update({ "system.traits": traits });
           });
         }
-        data.actor.update({ "system.traits": traits });
       }
     });
   } else {
@@ -271,23 +270,23 @@ export function rollXp(data) {
     }
     if (paid) {
       const target = trait.value * 2 + 6;
-      const roll = new Roll("2d10");
-      roll.evaluate({ async: false });
-      if (roll.total > target) {
-        trait.xp++;
-        if (trait.xp > 4) {
-          trait.value++;
-          trait.xp -= 5;
+      new Roll("2d10").evaluate().then((roll) => {
+        if (roll.total > target) {
+          trait.xp++;
+          if (trait.xp > 4) {
+            trait.value++;
+            trait.xp -= 5;
+          }
         }
-      }
-      const label = `Roll for XP on ${trait.label} with value of ${trait.value}. Needs a result over ${target}.`;
-      roll.toMessage({
-        speaker: ChatMessage.getSpeaker({ actor: data.actor }),
-        flavor: label,
-        rollMode: game.settings.get("core", "rollMode"),
+        const label = `Roll for XP on ${trait.label} with value of ${trait.value}. Needs a result over ${target}.`;
+        roll.toMessage({
+          speaker: ChatMessage.getSpeaker({ actor: data.actor }),
+          flavor: label,
+          rollMode: game.settings.get("core", "rollMode"),
+        });
+        data.actor.update({ "system.traits": traits });
       });
     }
-    data.actor.update({ "system.traits": traits });
   }
 }
 
@@ -295,52 +294,54 @@ export function rollChip(actor, dice = 1) {
   const chips = actor.system.chips;
   const total =
     chips.white + chips.green + chips.blue + chips.red + chips.black;
-  const roll = new Roll(`${dice}d6cs>${total}`);
-  roll.evaluate({ async: false });
-  const result = {
-    type: "d6",
-    dice: [],
-    successes: roll.total,
-    formula: `${dice}D6 against ${total} chips.`,
-    total: "No Chip",
-  };
-  for (const die of roll.dice[0].results) {
-    result.dice.push({ value: die.result, success: die.result > total });
-  }
-  if (roll.total > 0) {
-    const newChips = {
-      white: chips.white,
-      green: chips.green,
-      blue: chips.blue,
-      red: chips.red,
-      black: chips.black,
+  new Roll(`${dice}d6cs>${total}`).evaluate().then((roll) => {
+    const result = {
+      type: "d6",
+      dice: [],
+      successes: roll.total,
+      formula: `${dice}D6 against ${total} chips.`,
+      total: "No Chip",
     };
-    const chip = new Roll("1d5");
-    chip.evaluate({ async: false });
-    switch (chip.total) {
-      case 1:
-        newChips.white++;
-        result.total = "White Chip";
-        break;
-      case 2:
-        newChips.green++;
-        result.total = "Green Chip";
-        break;
-      case 3:
-        newChips.blue++;
-        result.total = "Blue Chip";
-        break;
-      case 4:
-        newChips.red++;
-        result.total = "Red Chip";
-        break;
-      case 5:
-        newChips.black++;
-        result.total = "Black Chip";
-        break;
+    for (const die of roll.dice[0].results) {
+      result.dice.push({ value: die.result, success: die.result > total });
     }
-    actor.update({ "system.chips": newChips });
-  }
-  const speaker = ChatMessage.getSpeaker({ actor });
-  showRoll(result, speaker, "Fate Chip");
+    const speaker = ChatMessage.getSpeaker({ actor });
+    if (roll.total > 0) {
+      const newChips = {
+        white: chips.white,
+        green: chips.green,
+        blue: chips.blue,
+        red: chips.red,
+        black: chips.black,
+      };
+      new Roll("1d5").evaluate().then((chip) => {
+        switch (chip.total) {
+          case 1:
+            newChips.white++;
+            result.total = "White Chip";
+            break;
+          case 2:
+            newChips.green++;
+            result.total = "Green Chip";
+            break;
+          case 3:
+            newChips.blue++;
+            result.total = "Blue Chip";
+            break;
+          case 4:
+            newChips.red++;
+            result.total = "Red Chip";
+            break;
+          case 5:
+            newChips.black++;
+            result.total = "Black Chip";
+            break;
+        }
+        actor.update({ "system.chips": newChips });
+        showRoll(result, speaker, "Fate Chip");
+      });
+    } else {
+      showRoll(result, speaker, "Fate Chip");
+    }
+  });
 }
